6e0c63af6309889419fd69a3835e3e712da99170
junmonthjun651 <54402566+junmonthjun651@users.noreply.github.com>
2019-08-23 19:49:26 +0900

    Update dfjk.py

--- dfjk.py
+++ dfjk.py
@@ -16,9 +16,9 @@ async def on_message(message):
     if message.content.startswith("/회원가입"):
         a = message.guild.get_member(int(message.author.id))
         b = discord.utils.get(message.guild.roles, name="회원")
-        await a.add_roles(b)
+        await add_roles(b)
         c = discord.utils.get(message.guild.roles, name="비회원")
-        await c.remove_roles(role)
+        await remove_roles(c)
     
 
     
